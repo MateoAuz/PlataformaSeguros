a484df76e3985ce704f45b475e8b641b3e17d41b
MateoAuz <teoauz@gmail.com>
2025-06-25 22:13:17 -0500

    login

--- front/src/pages/Login/Login.jsx
+++ front/src/pages/Login/Login.jsx
@@ -37,12 +37,12 @@ export const Login = () => {
 		}
 
 		try {
-			console.log("Enviando:", { correo: username, password });
+			console.log("Enviando:", { correo: username, password: password });
 
 			const res = await fetch(`${BaseUrl.BASE_URL}login`, {
 				method: "POST",
 				headers: { "Content-Type": "application/json" },
-				body: JSON.stringify({ correo: username, password })
+				body: JSON.stringify({ correo: username, password: password })
 			});
 
 			const data = await res.json();
